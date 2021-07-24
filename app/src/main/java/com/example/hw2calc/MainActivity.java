c45fa3ff8c5874b974b742cb149cdd0f597a57a8
ivanovusb <ivanov.aleksey.job@gmail.com>
2021-07-25 00:08:04 +1000

    Добавил все оставшиеся детали к калькулятору

--- app/src/main/java/com/example/hw2calc/MainActivity.java
+++ app/src/main/java/com/example/hw2calc/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.hw2calc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private int orientation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
